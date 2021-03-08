7ab91dbb5843ef3ab67b36032dd27b64bd8c826d
Matthew Lau <lau.cy.matthew@gmail.com>
2021-03-08 11:42:50 +0000

    Updated Lambda function

--- deploy/lambdas/processTodos/index.js
+++ deploy/lambdas/processTodos/index.js
@@ -2,13 +2,14 @@ const serverless = require("serverless-http");
 const bodyParser = require("body-parser");
 const express = require("express");
 const redis = require("redis");
+const { promisify } = require("util");
 const app = express();
 
 app.use(bodyParser.json({ strict: false }));
 
 const redisConfig = {
   // To-do: update host URL
-  host: "app-redis.kskdyd.ng.0001.use1.cache.amazonaws.com",
+  host: "test.kskdyd.0001.use1.cache.amazonaws.com",
   port: 6379
 };
 
@@ -18,22 +19,22 @@ app.get("/ping", function(req, res) {
 
 app.get("/set", function(req, res) {
   const client = redis.createClient(redisConfig);
-  client.set("key", "value");
+  client.on("ready", () => {
+    console.log(client.ping());
+  });
+  client.set("foo", "bar");
   res.send("Set!");
 });
 
-app.get("/get", function(req, res) {
+app.get("/get", async function(req, res) {
+  console.log("Getting...");
   const client = redis.createClient(redisConfig);
-  client.get("key", async (err, data) => {
-    if (err) {
-      res.send("Error");
-    }
-    if (data) {
-      res.send("Yes cache!");
-    } else {
-      res.send("No cache");
-    }
+  client.on("ready", () => {
+    console.log(client.ping());
   });
+  const getAsync = promisify(client.get).bind(client);
+  const val = await getAsync("foo");
+  res.send(val);
 });
 
 app.get("/todos", function(req, res) {
