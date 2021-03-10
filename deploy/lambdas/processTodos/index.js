3c3bdec42812d383449e38656fa1c23f4c3e421c
Matthew Lau <lau.cy.matthew@gmail.com>
2021-03-10 14:58:51 +0000

    Updated Lambda function to get data by Id

--- deploy/lambdas/processTodos/index.js
+++ deploy/lambdas/processTodos/index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(bodyParser.json({ strict: false }));
 
 const redisConfig = {
-  // To-do: update host URL
   host: process.env.REDIS_HOST_URL,
   port: 6379
 };
@@ -20,39 +19,92 @@ app.get("/ping", function(req, res) {
   res.send("pong!");
 });
 
-app.get("/set", function(req, res) {
-  const client = redis.createClient(redisConfig);
-  client.set("foo", "bar");
-  client.quit();
-  res.send("Set!");
+app.get("/v1/todos", function(req, res) {
+  const params = {
+    TableName: "todos"
+  };
+
+  dynamoDb.scan(params, (error, result) => {
+    if (error) {
+      res.status(404).json({ error: `Error when fetching todos: ${error}` });
+    } else {
+      res.status(200).json(result.Items);
+    }
+  });
 });
 
-app.get("/get", async function(req, res) {
+app.get("/v1/todos/:todoId", async function(req, res) {
+  const params = {
+    TableName: "todos",
+    Key: {
+      todoId: req.params.todoId
+    }
+  };
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      res.status(404).json({ error: `Error when fetching todo: ${error}` });
+    }
+    if (result.Item) {
+      const { todoId, todo } = result.Item;
+      res.json({ todoId, todo });
+    } else {
+      res.status(404).json({ error: "Todo not found" });
+    }
+  });
+});
+
+// Sets/gets data from ElastiCache Redis
+app.get("/v2/todos/:todoId", async function(req, res) {
+  const params = {
+    TableName: "todos",
+    Key: {
+      todoId: req.params.todoId
+    }
+  };
+
   const client = redis.createClient(redisConfig);
   const getAsync = promisify(client.get).bind(client);
-  const val = await getAsync("foo");
+  const val = await getAsync(req.params.todoId);
   client.quit();
-  res.send(val);
-});
 
-app.get("/todos", function(req, res) {
-  res.send("Get todos");
+  if (val) {
+    const { todoId, todo } = JSON.parse(val);
+    res.json({ todoId, todo });
+  } else {
+    dynamoDb.get(params, (error, result) => {
+      if (error) {
+        res.status(404).json({ error: `Error when fetching todo: ${error}` });
+      }
+      if (result.Item) {
+        const client = redis.createClient(redisConfig);
+        client.set(req.params.todoId, JSON.stringify(result.Item));
+        client.quit();
+        const { todoId, todo } = result.Item;
+        res.json({ todoId, todo });
+      } else {
+        res.status(404).json({ error: "Todo not found" });
+      }
+    });
+  }
 });
 
 app.post("/todos", function(req, res) {
   const params = {
     TableName: "todos",
     Item: {
-      todoId: "1",
-      name: "Buy food"
+      todoId: req.requestContext.requestId,
+      todo: req.body.todo
     }
   };
 
   dynamoDb.put(params, error => {
     if (error) {
-      res.send(error);
+      res.status(404).json({ error: `Error when creating todo: ${error}` });
     } else {
-      res.send("Done");
+      res
+        .status(200)
+        .json({ todoId: req.requestContext.requestId, todo: req.body.todo });
     }
   });
 });
